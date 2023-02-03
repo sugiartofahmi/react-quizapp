c7270018fa4ff74a35251f0df1181dde85ea0518
sugiartofahmi <sugiartofahmi@gmail.com>
2023-02-03 11:15:04 +0700

    Update Responsive

--- src/views/Quiz/index.jsx
+++ src/views/Quiz/index.jsx
@@ -149,7 +149,7 @@ const Quiz = () => {
                 {questions[currentQuestion].answerOptions.map(
                   (answerOption) => (
                     <button
-                      className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-xs px-5 py-2.5 text-center mr-2 mb-2"
+                      className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                       onClick={() =>
                         handleAnswerOptionClick(answerOption.isCorrect)
                       }
